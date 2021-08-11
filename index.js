15f262cd02719456a2d583a7b672127dc7502cc5
Rachel <ralsh39@gmail.com>
2021-08-11 22:43:45 +0800

    removed other intents to focuson operation hours intent

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 const admin = require('firebase-admin')
 
 // import orderBouquet from "./intents/order-bouquet.js"
+const orderBouquet = require('./intents/order-bouquet.js')
 
 var serviceAccount= {
   "type": process.env.account_type,
@@ -82,23 +83,22 @@ function handleDialog(req, res) {
     /*
     Thank you for placing your order. We will send your order of #bouquet-order.order-quantity bouquet(s) to $delivery-address on #bouquet-order.date-time.original . Please note that cash payment of SGD70 per bouquet should be made upon delivery. What else would you like to do?
     */
-    if (intent === "bouquet-order-collection") {
+    if (intent === "operation-hours") {
       const parameters = body.queryResult.parameters;
       const order_datetime = parameters["date-time"].original;    
       // const name = parameters["name"];
       // const venue = parameters["venue"];
   
-      const refAppointment = db.ref("appointments")
-      const newAppointmentRef = refAppointment.push()
+      const refOrder = db.ref("orders")
+      const newOrderRef = refAppointment.push()
       const store = parameters.store;
   
-      const refOrder = db.ref("orders")
-      refOrder.orderByChild('order').equalTo(order).on('value', function(snapshot) {
-        const clinics = snapshot.val()
+      
+      // refOrder.orderByChild('order_datetime').equalTo(order_datetime).on('value', function(snapshot) {
+        // const clinics = snapshot.val()
         
-          let message = "Thank you for placing your order! Please pick up from our pop-up store at The Wired Monkey cafe @ 5 Dunlop St, #01-00, Singapore 209335 (view on Google maps here: https://g.page/TheWiredMonkey?share). We will notify you when your order is ready. Also, please make payment of SGD70 per bouquet upon pick-up on " + order_datetime + ". What else would you want to do?"
-          // const keys = Object.keys(clinics)
-        
+          // let message = "Thank you for placing your order! Please pick up from our pop-up store at The Wired Monkey cafe @ 5 Dunlop St, #01-00, Singapore 209335 (view on Google maps here: https://g.page/TheWiredMonkey?share). We will notify you when your order is ready. Also, please make payment of SGD70 per bouquet upon pick-up on " + order_datetime + ". What else would you want to do?"
+          let message = "Our pop-up store is open on Mondays to Saturdays, 10am to 8pm, except on Public Holidays. Visit us at The Wired Monkey cafe @ 5 Dunlop St, #01-00, Singapore 209335 (view on Google maps here: https://g.page/TheWiredMonkey?share)! You can still place an order or book an appointment with us here. What would you like to do?"
           // let optionButtons = ['Book an Appointment', 'Purchase a Bouquet', 'End Chat']
         
           res.send({
@@ -116,6 +116,8 @@ function handleDialog(req, res) {
                     "subtitle": "The Wired Monkey @ 5 Dunlop St, #01-00, Singapore 209335 (https://g.page/TheWiredMonkey?share)",
                     "imageUri": "https://images.unsplash.com/photo-1567696153798-9111f9cd3d0d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"//,
                     // "buttons": optionButtons
+                    // try custom payloads (buttons to trigger to another intent or hyperlink)
+                    // telegram should convert text to links if it's a url-> to check
                   },
                   "reply_markup": {
                     "inline_keyboard": [
@@ -146,122 +148,12 @@ function handleDialog(req, res) {
         
         
        
-      })
+      // }
+      )
     }
       res.status(200).send()    
-    } else
-    if (intent === "make-appointment") {
-      const parameters = body.queryResult.parameters;
-      const appointment_datetime = parameters["appointment-datetime"].date_time;    
-      const name = parameters["name"];
-      const venue = parameters["venue"];
-  
-      const refAppointment = db.ref("appointments")
-      const newAppointmentRef = refAppointment.push()
-
-      /*
-      Have fixed slots e.g. 9am, 10am, etc.
-      Create a dictionary. Write it out
-      JSON object:
-      {
-        appointment_datetime: appointment_datetime, //index can be a string
-        name: name,
-        phone_number: phone_number,
-        venue: venue
-      
-      }
-      
-      Use time as index
-        */
-      // as a list
-      newAppointmentRef.set({
-        name: name,
-        venue: venue,
-        appointment_datetime: appointment_datetime
-        // email: email
-      })
-      res.status(200).send()    
-    } else
-    if (intent === "bouquet-order") {
-      const parameters = body.queryResult.parameters;
-
-      const quantity = parameters["order-quantity"]
-      const order_datetime = parameters["date-time"].date_time;    
-      const custom_order = parameters["custom-order"];
-      // const name = parameters["name"];
-      // const venue = parameters["venue"];
-      
-  
-      const refOrder = db.ref("orders")
-      const newOrderRef = refOrder.push()
-
-      newOrderRef.set({
-        quantity: quantity,
-        order_datetime: order_datetime,
-        custom_order: custom_order
-      })
-      /*
-      const town = parameters.town;
-  
-      const refClinics = db.ref("clinics")
-      refClinics.orderByChild('town').equalTo(town).on('value', function(snapshot) {
-        const clinics = snapshot.val()
-        
-        if(clinics !== undefined && clinics !== null) {
-          let message = ''
-          const keys = Object.keys(clinics)
-        
-          let clinicButtons = []
-  
-          for(let i = 0; i < keys.length; i++) {
-            let address = clinics[keys[i]].address
-            let name = clinics[keys[i]].name
-            message = message + name + " at " + address + "\r\n" // Health Way at Parkway Parade 01-04
-  
-            clinicButtons.push({
-              text: name,
-              postback: "https://www.google.com/maps/search/?api=1&query=" + address
-            })
-          }
-        } else{
-          res.send({
-              "fulfillmentMessages": [
-                {
-                  "text": {
-                    "text": [
-                      message
-                    ]
-                  }
-                },
-                {
-                  "card": {
-                    "title": "Clinics Available",
-                    "subtitle": "",
-                    "imageUri": "https://images.unsplash.com/photo-1505751172876-fa1923c5c528?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-                    "buttons": clinicButtons
-                  },
-                  "platform": "TELEGRAM"
-                },
-              ]
-          })
-        }
-        
-       
-      })
-      */
     }
-  }
   
   app.post('/appointment', handleAppointment)
   app.post('/dialog', handleDialog)
 
-/*
-// JSON structure:
-appointment: {
-    first_name: "",
-    last_name: "",
-    phone: "",
-    time: "",
-    location: ""
-}
-*/
