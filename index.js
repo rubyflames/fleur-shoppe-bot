fabbd294a0b3bceb0d40e1c03ae93874a85e9298
Rachel <ralsh39@gmail.com>
2021-08-14 13:03:59 +0800

    rearrange telegram response

--- index.js
+++ index.js
@@ -104,18 +104,17 @@ function handleDialog(req, res) {
         },
         {
           "telegram": {
-
-            "text": {
-              "text": [
-                message
-            ]
-          },
           "card": {
             "title": "Our Pop-up Store",
             "subtitle": "The Wired Monkey @ 5 Dunlop St, #01-00, Singapore 209335 (https://g.page/TheWiredMonkey?share)",
             "imageUri": "https://images.unsplash.com/photo-1567696153798-9111f9cd3d0d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
             "buttons": optionButtons
             
+          },
+          "text": {
+            "text": [
+              message
+            ]
           },
           "reply_markup": {
             "inline_keyboard": [
